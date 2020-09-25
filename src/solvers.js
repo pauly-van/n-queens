44eede4ffd4a87caffac6c7c56c9715d784f049b
Paul Nguyen <paulngu1120@gmail.com>
2020-09-25 17:01:22 +0000

    psuedocode for nRooks

--- src/solvers.js
+++ src/solvers.js
@@ -69,17 +69,81 @@ e: ?
   // if iterate through whole board then return solution
 
 
-
-
-
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  //output: number — count of solutinos for board of n size
+  //input: n — board size 
+  //constraints: n is an integer
+  // edge cases: find all possible solutions
 
+  //As the solutions function finds an open space on the board, 
+  //we should be able to add a piece, yet also go back to this point of our function and let it carry out for the next possible open space (this is to determine other possible spaces at the certain point in execution) 
+  // let the function go until the end for each iteration / possible solution, and increment our solutionCount each time
+
+
+  //work: seeing if can put piece down or not
+  // togglePiece at current row and col
+  // if board.hasRowConflictAt current row or board.hasColConflictAt current col then we togglePiece again -- skip space
+  // 
+
+
+  /* 2nd explanation 
+1. first we want to put a piece on first available spot on first row,
+2. then we want to go into 2nd row and place a piece and see if theres a conflict, if there is a conflict move piece to next col
+and check collision... repeat until place is found or end of col index
+3. Once a place is found continue down the rows until all pieces are placed and add to solutions count 
+4. repeat steps 1-3 for the first piece placed on board until end of column index
+5. return the count
+*/
+  
+  // 
+  var solutionCount = 0; 
+  let board = new Board({n});
+  //create a function that repeats placing pieces on rows called findSpot
+  // lets say nRooksPlaced = 0 (when this reaches n stop recursing)
+  // lets say boardview is the matrix using .rows()
+  // traverse thru row to see if there an empty spot
+  // if space is avail and no conflicts
+  // place a piece
+  // nRooksPlaced increment
+  // call findSpot on next row
+  // otherwise 
+  // continue to next element in row
+  // place a piece/check for conflicts... and so on
+  // if no space can be found 
+  // nRooksPlaced decrement
+  // reset prev row to [0,0.... n] (starting at last index?)
+  // call findSpot on prev row 
+          
+
+  let recursiveSpot = function(rowIndex) {
+    // so if row is equal n-1 and an piece is placed
+    // increment solutions count
+    // return (to stop recursing)
+    // toggle piece on rowIndex colIndex
+    // check for conflicts
+    // if there are no conflicts
+    // invoke recursiveSpot on next row
+    
+  };
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+  recursiveSpot(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
